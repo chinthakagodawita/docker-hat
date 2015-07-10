815ef7475cd4be76750cb1788ffbc9ccb8783938
Chinthaka Godawita <chin@sitback.com.au>
2015-07-10 17:42:42 +1000

    Initial WIP of node-based helpers.

---
+++ bin/dh.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+'use strict';
+
+var
+  shell,
+  yargs,
+  argv;
+
+shell = require('shelljs');
+yargs = require('yargs');
+
+argv = yargs.usage("$0 command")
+  .command("init", "Install boot2docker.", function (yargs) {
+    require('./init.js').run();
+  })
+  .command("start", "push changes up to GitHub", function (yargs) {
+    require('./start.js').run();
+  })
+  .command("stop", "commit and push changes in one step", function (yargs) {
+
+  })
+  .command("restart", "commit and push changes in one step", function (yargs) {
+    
+  })
+  .command("proxy", "commit and push changes in one step")
+  .demand(1, "must provide a valid command")
+  .help("h")
+  .alias("h", "help")
+  .argv
+
