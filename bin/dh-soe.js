2909eef13d4f203fd02774cd4683e952e3c75e18
Chinthaka Godawita <chin@sitback.com.au>
2015-08-14 09:22:42 +1000

    Adds wrapper for Drush under soe.
    Part of #16.

--- bin/dh-soe.js
+++ bin/dh-soe.js
@@ -6,6 +6,7 @@ var
   yargs,
   libSoe,
   libUtils,
+  libDocker,
   argv,
   cmd;
 
@@ -13,6 +14,7 @@ shell = require('shelljs');
 yargs = require('yargs');
 libSoe = require('../lib/soe');
 libUtils = require('../lib/utils');
+libDocker = require('../lib/docker');
 
 argv = yargs.usage('dh soe <command>')
   .command('start', 'Start a SOE container', function (yargs) {
@@ -40,6 +42,37 @@ argv = yargs.usage('dh soe <command>')
   .command('restart', 'Restart a SOE container', function (yargs) {
     libSoe.restart(libUtils.common.getYargsContainerInfo(yargs, 'restart'));
   })
+  .command('drush', 'Run Drush inside a SOE container', function (yargs) {
+    // @TODO, shell exec a separate program, we need to make yargs isn't
+    // running. It interferes with arguments
+    var
+      subArgv,
+      cmd;
+
+    subArgv = yargs.reset()
+    .usage('dh soe drush <container name> [<commands>] [options]')
+    .option('d', {
+      alias: 'debug',
+      description: 'Display debug messages.'
+    })
+    .help('help')
+    .argv;
+
+    if (!subArgv._[1]) {
+      throw new Error('please provide a container name');
+    }
+
+    // @todo: move this into a library.
+    cmd = [
+      'drush',
+      '--root=/var/www'
+    ];
+    if (subArgv._.length > 1) {
+      cmd = cmd.concat(subArgv._.slice(2));
+    }
+
+    libDocker.exec.run(subArgv._[1], cmd, '-it');
+  })
   .option('d', {
     alias: 'debug',
     description: 'Display debug messages.'
@@ -48,6 +81,7 @@ argv = yargs.usage('dh soe <command>')
   .example('dh soe start mysite.dev', 'Start a container with the hostname "mysite.dev" using sources from the current directory.')
   .example('dh soe stop mysite.dev', 'Stops the "mysite.dev" container if it exists.')
   .example('dh soe restart mysite.dev', 'Stops the "mysite.dev" container if it exists and starts it back up using sources from the current directory.')
+  .example('dh soe drush mysite.dev cc all', 'Clears the Drupal cache in the "mysite.dev" container via Drush.')
   .check(function (argv, opts) {
     if (!argv._[0].match(/start|stop|restart/)) {
       throw new Error('please provide a valid command');
